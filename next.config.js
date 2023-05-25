69f1fe32c9918493f6fca84eed358f47c9d51139
jaehyunjang <hyuni2020@gmail.com>
2023-05-25 13:34:38 +0900

    deploy test

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   reactStrictMode: true,
+  // Use the prefix for asset urls in next.js
+  assetPrefix: '/AI-Dev-Hub/',
+  basePath: '/AI-Dev-Hub',
+  trailingSlash: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
