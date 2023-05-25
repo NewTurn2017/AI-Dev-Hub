fb7b9a33d5880a6af23bbaa53f872969dbe5e276
jaehyunjang <hyuni2020@gmail.com>
2023-05-25 11:42:50 +0900

    Updates

---
+++ _next/static/g08QaSsazyod148lLAQ9u/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/322-f8b5dcfb3b8737ef96cb.js","static/chunks/pages/index-47376f9bf396297ce065.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
